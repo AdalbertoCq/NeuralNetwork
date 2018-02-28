Fixed batch normalization implementation

deep_neural_network_base.py
@@ -62,7 +62,7 @@ class NeuralNetwork():
         else:
             print('Last activation function not comteplated: %s' % self.activations[-1])
             exit(1)
-        cost = np.sum(loss, axis=1, keepdims=True) / Y.shape[1]
+        cost = np.sum(loss, axis=1, keepdims=True) / float(Y.shape[1])
         cost = np.squeeze(cost)
         return cost
 
@@ -73,8 +73,8 @@ class NeuralNetwork():
             dZ = np.multiply(dA, sigmoid_derivative(Z))
         elif activation == 'tanh':
             dZ = np.multiply(dA, tanh_derivative(Z))
-        dW = np.dot(dZ, A_prev.T)/A_prev.shape[1]
-        dB = np.sum(dZ, axis= 1, keepdims=True)/A_prev.shape[1]
+        dW = np.dot(dZ, A_prev.T)/float(A_prev.shape[1])
+        dB = np.sum(dZ, axis= 1, keepdims=True)/float(A_prev.shape[1])
         dA_prev = np.dot(W.T, dZ)
         return dA_prev, dW, dB
 
@@ -84,8 +84,8 @@ class NeuralNetwork():
         W = self.parameters['W%s' % str(self.num_layers)]
         A_prev = self.cache['A%s' % str(self.num_layers - 1)]
         dZ = AL - Y
-        self.grads['dW%s' % str(self.num_layers)] = np.dot(dZ, A_prev.T)/A_prev.shape[1]
-        self.grads['dB%s' % str(self.num_layers)] = np.sum(dZ, axis=1, keepdims=True)/A_prev.shape[1]
+        self.grads['dW%s' % str(self.num_layers)] = np.dot(dZ, A_prev.T)/float(A_prev.shape[1])
+        self.grads['dB%s' % str(self.num_layers)] = np.sum(dZ, axis=1, keepdims=True)/float(A_prev.shape[1])
         self.grads['dA%s' % str(self.num_layers - 1)] = np.dot(W.T, dZ)
 
         # Starts by L-1

deep_neural_network_batch_normalization.py
@@ -43,18 +43,26 @@ class NeuralNetwork(dnn.NeuralNetwork):
             self.cache['A%s' % l], self.cache['Y%s' % l], self.cache['Z%s' % l] = self.linear_forward(self.cache['A%s' % str(l-1)], G, B, W, activation_layer)
 
     def normalize_backwards(self, dY, Z, G):
-        m = Z.shape[1]
+        m = float(Z.shape[1])
+        mean = np.mean(Z, axis=1, keepdims=True)
         var = np.var(Z, axis=1, keepdims=True)
-        std = 1. / np.sqrt(var + self.eps_norm)
-        Z_norm = normalize(Z, self.eps_norm)
-        dB = np.sum(dY, axis=1, keepdims=True) / m
-        dG = np.sum(dY * Z_norm, axis=1, keepdims=True) / m
+        std = 1./np.sqrt(var + self.eps_norm)
+
         dZ_norm = dY * G
-        dZ = (1. / m) * std * (m * dZ_norm - np.sum(dZ_norm, axis=1, keepdims=True) - Z_norm * np.sum(dZ_norm * Z_norm, axis=1, keepdims=True))
+        Z_norm = normalize(Z, self.eps_norm)
+        first_term = m * dZ_norm
+        second_term = np.sum(dZ_norm, axis=1, keepdims=True)
+        third_term_a = (Z - mean) * (np.power(std, 2))
+        third_term_b = np.sum(dZ_norm * (Z - mean), axis=1, keepdims=True)
+        third_term = third_term_a * third_term_b
+        dZ = (1./m) * std * (first_term - second_term - third_term)
+        dG = np.sum(dY * Z_norm, axis=1, keepdims=True)/m
+        dB = np.sum(dY, axis=1, keepdims=True)/m
+
         return dZ, dG, dB
 
     def linear_backwards(self, dA, A_prev, Y, Z, G, W, activation):
-        m = Z.shape[1]
+        m = float(Z.shape[1])
         if activation == 'relu':
             dY = np.multiply(dA, relu_derivative(Y))
         elif activation == 'sigmoid':
@@ -73,8 +81,8 @@ class NeuralNetwork(dnn.NeuralNetwork):
         if activation_layer == 'softmax':
             A_prev = self.cache['A%s' % str(self.num_layers - 1)]
             dZ = AL - Y
-            dW_temp = np.dot(dZ, A_prev.T)/A_prev.shape[1]
-            db_temp = np.sum(dZ, axis=1, keepdims=True)/A_prev.shape[1]
+            dW_temp = np.dot(dZ, A_prev.T)/float(A_prev.shape[1])
+            db_temp = np.sum(dZ, axis=1, keepdims=True)/float(A_prev.shape[1])
             dA_prev_temp = np.dot(W.T, dZ)
         elif activation_layer == 'sigmoid':
             dAL = -np.divide(Y, AL) + np.divide(1-Y, 1-AL)