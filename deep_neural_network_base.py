Fixing gradients

deep_neural_network_base.py
@@ -83,10 +83,18 @@ class NeuralNetwork():
         AL = self.cache['A%s' % str(self.num_layers)]
         W = self.parameters['W%s' % str(self.num_layers)]
         A_prev = self.cache['A%s' % str(self.num_layers - 1)]
-        dZ = AL - Y
-        self.grads['dW%s' % str(self.num_layers)] = np.dot(dZ, A_prev.T)/float(A_prev.shape[1])
-        self.grads['dB%s' % str(self.num_layers)] = np.sum(dZ, axis=1, keepdims=True)/float(A_prev.shape[1])
-        self.grads['dA%s' % str(self.num_layers - 1)] = np.dot(W.T, dZ)
+        if self.activations[-1] == 'softmax':
+            dZ = AL - Y
+            dW = np.dot(dZ, A_prev.T)/float(A_prev.shape[1])
+            dB = np.sum(dZ, axis=1, keepdims=True)/float(A_prev.shape[1])
+            dA_prev = np.dot(W.T, dZ)
+        else:
+            Z = self.parameters['Z%s' % str(self.num_layers)]
+            dAL = - (1./AL.shape[1])*np.divide(Y,AL)
+            dA_prev, dW, dB = self.linear_backwards(self, dAL, A_prev, Z, W, self.activations[-1])
+        self.grads['dW%s' % str(self.num_layers)] = dW
+        self.grads['dB%s' % str(self.num_layers)] = dB
+        self.grads['dA%s' % str(self.num_layers - 1)] = dA_prev
 
         # Starts by L-1
         for l in reversed(range(1, self.num_layers)):

deep_neural_network_batch_normalization.py
@@ -83,7 +83,20 @@ class NeuralNetwork(dnn.NeuralNetwork):
         G = self.parameters['G%s' % str(self.num_layers)]
         activation_layer = self.activations[self.num_layers]
         A_prev = self.cache['A%s' % str(self.num_layers - 1)]
-        dZ, dG_temp, db_temp = self.normalize_backwards(AL - Y, Z, G)
+
+        if self.activations[-1] == 'softmax':
+            dY = AL - Y
+        elif self.activations[-1] == 'sigmoid':
+            dAL = - (1. / AL.shape[1]) * np.divide(Y, AL)
+            dY = np.multiply(dAL, sigmoid_derivative(Y))
+        elif self.activations[-1] == 'relu':
+            dAL = - (1. / AL.shape[1]) * np.divide(Y, AL)
+            dY = np.multiply(dAL, relu_derivative(Y))
+        elif self.activations[-1] == 'tanh':
+            dAL = - (1. / AL.shape[1]) * np.divide(Y, AL)
+            dY = np.multiply(dAL, tanh_derivative(Y))
+
+        dZ, dG_temp, db_temp = self.normalize_backwards(dY, Z, G)
         dW_temp = np.dot(dZ, A_prev.T)/float(A_prev.shape[1])
         dA_prev_temp = np.dot(W.T, dZ)
         self.grads['dA%s' % str(self.num_layers - 1)] = dA_prev_temp

sandbox_batch_norm.py
@@ -5,7 +5,7 @@ from mnist.loader import MNIST
 
 
 # Loading data.
-db = '/Users/aclaudioquiros/Documents/neural_netwoks/Data/MNIST_database/'
+db = '/Users/aclaudioquiros/Documents/NN Data/Data/MNIST_database/'
 mndata = MNIST(db)
 images, labels = mndata.load_training()
 images_test, labels_test = mndata.load_testing()
@@ -29,7 +29,7 @@ images_test = normalize(images_test)
 
 layer_dim = [images.shape[0], 125, 40, labels.shape[0]]
 activations = [None, 'relu', 'relu', 'softmax']
-deep_nn = NeuralNetwork(layer_dim, activations, learning_rate=0.02, num_iterations=500, mini_batch_size=256, eps_norm=1e-8)
+deep_nn = NeuralNetwork(layer_dim, activations, learning_rate=0.2, num_iterations=100, mini_batch_size=256, eps_norm=1e-8)
 deep_nn.train_set(images, labels)
 
 mean = np.mean(np.concatenate((images, images_test), axis=1))